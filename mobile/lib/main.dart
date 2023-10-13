d08e36ad61447b9dcfb6aecee1e4fde1d4c37549
fauzimaulanaalam <alamfauzi000@gmail.com>
2023-10-13 21:52:02 +0700

    Setting splash screen mobile

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:mobile/screen/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,73 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter MySQL Example',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() {
-    // Avoid using private types in public APIs.
-    return _HomeScreenState();
-  }
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String data = ''; // Store the fetched data
-
-  Future<void> fetchData() async {
-    // Replace with your API endpoint
-    final apiUrl =
-        Uri.parse('http://10.60.5.168:3000/api/data'); // Change to your API URL
-    final response = await http.get(apiUrl);
-
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      setState(() {
-        data = jsonData['message']; // Adjust to your API response structure
-      });
-    } else {
-      setState(() {
-        data = 'Failed to fetch data';
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter MySQL Example'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Data from MySQL:',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              data,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: fetchData,
-              child: const Text('Fetch Data'),
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
