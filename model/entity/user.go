1939a2ad72964263957ae96c00e2526f11780efd
fauzimaulanaalam <alamfauzi000@gmail.com>
2023-10-28 10:31:09 +0700

    Adding OpenApi JSON

--- model/entity/user.go
+++ model/entity/user.go
@@ -3,6 +3,7 @@ package entity
 type User struct {
 	Id       int    `json:"id"`
 	NIP      string `json:"nip"`
+	Name     string `json:"name"`
 	Rank     int    `json:"rank"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
