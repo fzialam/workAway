70477bd8c1cafb467e42888f3971e2ebe081ab32
fauzimaulanaalam <alamfauzi000@gmail.com>
2023-11-11 06:35:16 +0700

    Setting configuration entity surat_tugas

--- model/entity/participan.go
+++ model/entity/participan.go
@@ -5,13 +5,3 @@ type Participan struct {
 	UserId       []int `json:"user_id"`
 	SuratTugasId int   `json:"surat_tugas_id"`
 }
-
-type ParticipanJoinUser struct {
-	Id           int    `json:"id"`
-	UserId       int    `json:"user_id"`
-	SuratTugasId int    `json:"surat_tugas_id"`
-	NIP          string `json:"nip"`
-	Name         string `json:"name"`
-	NoTelp       string `json:"no_telp"`
-	Email        string `json:"email"`
-}
