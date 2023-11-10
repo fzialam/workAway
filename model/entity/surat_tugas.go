3b7c3228aac1dc5998b606d6d535d2e21371fe40
fauzimaulanaalam <alamfauzi000@gmail.com>
2023-11-11 06:27:59 +0700

    Setting configuration entity surat_tugas

--- model/entity/surat_tugas.go
+++ model/entity/surat_tugas.go
@@ -15,18 +15,3 @@ type SuratTugas struct {
 	TglAkhir         string    `json:"tgl_akhir"`
 	Create_at        time.Time `json:"create_at"`
 }
-
-type SuratTugasJOINApproved struct {
-	Id               int       `json:"id"`
-	UserId           int       `json:"user_id"`
-	LokasiTujuan     string    `json:"lokasi_tujuan"`
-	JenisProgram     int       `json:"jenis_program"`
-	DokumenName      string    `json:"dokumen_name"`
-	DokumenPDF       string    `json:"dokumen_pdf"`
-	DokPendukungName string    `json:"dok_pendukung_name"`
-	DokPendukungPdf  string    `json:"dok_pendukung_pdf"`
-	TglAwal          string    `json:"tgl_awal"`
-	TglAkhir         string    `json:"tgl_akhir"`
-	Create_at        time.Time `json:"create_at"`
-	Status           int       `json:"status"`
-}
